036c4a51cccd8eebd6704201486f12b538252e32
Shruti <18it.shruti.santoki@gmail.com>
2022-02-03 11:47:32 +0530

    Using Comparable Interface

--- student.java
+++ student.java
@@ -1,48 +1,34 @@
+import java.util.*;
 import java.util.ArrayList;
 
 public class student{ 
      
      public static void main (String[] args){
 
-          Person p1 = new Person("Anuj", 25, "anuj01@gmail.com", 73);
-          Person p2 = new Person("Binod", 19, "binod02@gmail.com", 87);
-          Person p3 = new Person("Chandu", 34, "chandu03@gmail.com", 66);
-          Person p4 = new Person("Dholu", 28, "dholu04@gmail.com", 55);
-          Person p5 = new Person("Ezz", 15, "ezz05@gmail.com", 92);
-
          ArrayList<Person> names = new ArrayList<Person>();
 
-             names.add(p1);
-             names.add(p2);
-             names.add(p3);
-             names.add(p4);
-             names.add(p5);
+             names.add(new Person("Chandu", 25, "chandu01@gmail.com", 73));
+             names.add(new Person("Binod", 19, "binod02@gmail.com", 87));
+             names.add(new Person("Ezz", 34, "ezz03@gmail.com", 66));
+             names.add(new Person("Dholu", 28, "dholu04@gmail.com", 55));
+             names.add(new Person("Anuj", 15, "anuj05@gmail.com", 92));
 
-         System.out.println(names.get(0).name);
-         System.out.println(names.get(0).age);
-         System.out.println(names.get(0).email);
-         System.out.println(names.get(0).marks);
-         System.out.println();
-         System.out.println(names.get(1).name);
-         System.out.println(names.get(1).age);
-         System.out.println(names.get(1).email);
-         System.out.println(names.get(1).marks);
-         System.out.println();
-         System.out.println(names.get(2).name);
-         System.out.println(names.get(2).age);
-         System.out.println(names.get(2).email);
-         System.out.println(names.get(2).marks);
-         System.out.println();
-         System.out.println(names.get(3).name);
-         System.out.println(names.get(3).age);
-         System.out.println(names.get(3).email);
-         System.out.println(names.get(3).marks);
-         System.out.println();
-         System.out.println(names.get(4).name);
-         System.out.println(names.get(4).age);
-         System.out.println(names.get(4).email);
-         System.out.println(names.get(4).marks);
+             System.out.println("Unsorted");
+         for(Person p : names){
+             System.out.println(p.getname() + "" + p.getage() + "" + p.getemail() + "" + p.getmarks());
+         }
 
+         Collections.sort(names, new Sortbyname());
+         System.out.println("\nSorted by name");
+         for(Person p : names){
+            System.out.println(p.getname() + "" + p.getage() + "" + p.getemail() + "" + p.getmarks());
+         }
+
+        Collections.sort(names, new Sortbyage());
+        System.out.println("\nSorted by age");
+         for(Person p : names){
+            System.out.println(p.getname() + "" + p.getage() + "" + p.getemail() + "" + p.getmarks());
+         }
      }
 }
 
@@ -59,6 +45,26 @@ class Person {
          this.age = age;
          this.email = email;
          this.marks = marks;
-
      }
+
+     public String getname(){ return name;}
+     public int getage(){ return age;}
+     public String getemail(){ return email;}
+     public int getmarks(){ return marks;}
+}
+
+class Sortbyname implements Comparator<Person> {
+
+    public int compare(Person a, Person b){
+
+        return a.name.compareTo(b.name);
+    }
+}
+
+class Sortbyage implements Comparator<Person> {
+
+    public int compare(Person a, Person b) {
+
+        return Integer.compare(a.age, b.age);
+    }
 }
